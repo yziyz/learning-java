fc8b0a246bc6622799ccf00f13b7a54466793186
yuanzhen <yziyz@foxmail.com>
2017-07-24 10:32:19 +0800

    指定集合初始值大小

--- src/main/java/yz/learning/collection/NullInMap.java
+++ src/main/java/yz/learning/collection/NullInMap.java
@@ -10,11 +10,14 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class NullInMap {
     public static void main(String[] args) {
-        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        /*
+        集合初始化时，指定集合初始值大小。initialCapacity = (需要存储的元素个数 / 负载因子) + 1。注意负载因子（即 loader factor）默认为 0.75，如果暂时无法确定初始值大小，请设置为 16。
+         */
+        Hashtable<String, Integer> hashtable = new Hashtable<>(16);
         hashtable.put(null, 0);//java.lang.NullPointerException
         hashtable.put("null", null);//java.lang.NullPointerException
 
-        ConcurrentHashMap<String, Integer> concurrentHashMap = new ConcurrentHashMap<>();
+        ConcurrentHashMap<String, Integer> concurrentHashMap = new ConcurrentHashMap<>(16);
         concurrentHashMap.put(null, 1);//java.lang.NullPointerException
         concurrentHashMap.put("null", null);//java.lang.NullPointerException
 
@@ -22,7 +25,7 @@ public class NullInMap {
         treeMap.put("null", null);
         treeMap.put(null, 1);//java.lang.NullPointerException
 
-        HashMap<String, Integer> hashMap = new HashMap<>();
+        HashMap<String, Integer> hashMap = new HashMap<>(16);
         hashMap.put(null, 1);
         hashMap.put("null", null);
         hashMap.put(null, null);
