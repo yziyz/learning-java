bd6df4e6242a93eb95200785daf8ee802727f22d
yuanzhen <yziyz@foxmail.com>
2017-07-28 14:16:49 +0800

    使用java.util.Objects#equals判空

--- src/main/java/yz/learning/basic/DetermineNull.java
+++ src/main/java/yz/learning/basic/DetermineNull.java
@@ -4,14 +4,19 @@ import java.util.Objects;
 
 /**
  * @author 袁臻
- * 17-7-27
- *
- * 判空
+ * 2017-07-28 13:09
  */
 public class DetermineNull {
     public static void main(String[] args) {
-        final String type = "city";
-        System.out.println(Objects.equals(null, type));
-        System.out.println(Objects.equals(null, ""));
+        //新建非空字符串string
+        final String notNullString = "Hello World!";
+        final String nullString = null;
+        //判断string是否为null
+        //System.out.println(nullString.equals(notNullString));//此处抛出NPE
+        System.out.println(Objects.equals(nullString, notNullString));//此处没有NPE，false
+        //System.out.println(nullString.equals(null));//此处抛出NPE
+        System.out.println(Objects.equals(nullString, null));//此处没有NPE，true
+        System.out.println(Objects.equals(null, null));//此处没有NPE，true
+        System.out.println(Objects.equals(notNullString, "Hello World!"));//true
     }
 }
