842d342c8cabb9e89c05b60d6799da9f8969eb34
yuanzhen <yziyz@foxmail.com>
2017-08-19 15:25:04 +0800

    字符串拼接

---
+++ src/main/java/yz/learning/basic/StringConcat.java
@@ -0,0 +1,13 @@
+package yz.learning.basic;
+
+/**
+ * @author 袁臻
+ * 2017/08/17 13:25
+ */
+public class StringConcat {
+    public static void main(String[] args) {
+        String string = "Hello ";
+        string = string.concat("World!");
+        System.out.println(string);
+    }
+}
