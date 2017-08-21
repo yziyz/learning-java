0f1ad0f8096531e9a3ecd0c4635815d8732b39d3
yuanzhen <yziyz@foxmail.com>
2017-08-21 11:58:05 +0800

    捕获多个异常

--- src/main/java/yz/learning/exception/HandleMoreThanOneExceptions.java
+++ src/main/java/yz/learning/exception/HandleMoreThanOneExceptions.java
@@ -1,16 +1,33 @@
 package yz.learning.exception;
 
+import java.lang.reflect.Modifier;
+
 /**
+ * 在单个catch语句中捕获多种类型的异常时，类型对象参数被隐形设置为final
+ *
  * @author 袁臻
  * 2017/08/19 15:18
  */
 public class HandleMoreThanOneExceptions {
     public static void main(String[] args) {
+        //单个catch语句中捕获多种类型的异常
         try {
-            System.out.println("这是一个空操作");
+            //抛出异常
+            throw new ArithmeticException();
         } catch (ArithmeticException | NullPointerException e) {
-            //multi-catch parameter e may not be assigned
-            //e = new ArithmeticException();
+            int modifier = e.getClass().getModifiers();
+            //打印修饰符
+            System.out.println(Modifier.toString(modifier));
+        }
+
+        //单个catch语句中捕获一种类型的异常
+        try {
+            //抛出异常
+            throw new Exception();
+        } catch (Exception e) {
+            int modifier = e.getClass().getModifiers();
+            //打印修饰符
+            System.out.println(Modifier.toString(modifier));
         }
     }
 }
