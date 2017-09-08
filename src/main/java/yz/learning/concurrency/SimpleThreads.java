857b06a6219e3ed6a7fd4c4a6c1edd69a9e47666
yuanzhen <yziyz@foxmail.com>
2017-09-08 09:18:06 +0800

    学习线程

--- src/main/java/yz/learning/concurrency/SimpleThreads.java
+++ src/main/java/yz/learning/concurrency/SimpleThreads.java
@@ -31,6 +31,7 @@ public class SimpleThreads {
                     threadMessage(anImportantInfo);
                 }
             } catch (InterruptedException e) {
+                System.err.print(Thread.currentThread().getName() + ": ");
                 e.printStackTrace();
             }
         }
@@ -38,7 +39,7 @@ public class SimpleThreads {
 
     public static void main(String[] args)
             throws InterruptedException {
-        long patience = 1000 * 60 * 60;
+        long patience = 1000L * 5L;
 
         if (args.length > 0) {
             patience = Long.parseLong(args[0]) * 1000;
@@ -47,15 +48,18 @@ public class SimpleThreads {
         threadMessage("Starting MessageLoop thread");
         long startTime = System.currentTimeMillis();
         Thread t = new Thread(new MessageLoop());
+        t.setName("MessageLoop");
         t.start();
+
+        threadMessage("Waiting for MessageLoop to finish");
+        //循环直至MessageLoop线程结束
         while (t.isAlive()) {
             threadMessage("Still waiting...");
             // Wait maximum of 1 second
             // for MessageLoop thread
             // to finish.
             t.join(1000);
-            if (((System.currentTimeMillis() - startTime) > patience)
-                    && t.isAlive()) {
+            if (((System.currentTimeMillis() - startTime) > patience) && t.isAlive()) {
                 threadMessage("Tired of waiting!");
                 t.interrupt();
                 // Shouldn't be long now
@@ -63,5 +67,24 @@ public class SimpleThreads {
                 t.join();
             }
         }
+
+        threadMessage("Finished");
     }
 }
+
+/*
+main: Starting MessageLoop thread
+main: Waiting for MessageLoop to finish
+main: Still waiting...
+main: Still waiting...
+main: Still waiting...
+main: Still waiting...
+MessageLoop: Mares eat oats
+main: Still waiting...
+MessageLoop: java.lang.InterruptedException: sleep interrupted
+	at java.lang.Thread.sleep(Native Method)
+	at yz.learning.concurrency.SimpleThreads$MessageLoop.run(SimpleThreads.java:30)
+	at java.lang.Thread.run(Thread.java:748)
+main: Tired of waiting!
+main: Finished
+ */
